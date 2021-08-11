24a23e6513744376277b73136c52ef62b0fdac8a
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-08-11 15:51:18 -0500

    commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,11 @@
 import * as React from "react"
-import { graphql } from "graphql";
-import get from 'lodash/get'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 import Donate from "../components/donate";
 
 import HomeHero from '../components/HomeHero';
-import BiteSizeWidget from "../components/widgets/bite-size-widget";
 
-const videos = get(this, 'props.data.allContentfulVideoPost.edges')
 
 const IndexPage = () => (
   <Layout>
@@ -20,21 +16,3 @@ const IndexPage = () => (
 )
 
 export default IndexPage
-
-export const pageQuery = graphql`
-query IndexPageQuery {
-  allContentfulVideoPost {
-    edges {
-      node {
-        title
-        slug
-        thumbnail {
-          fluid(maxWidth: 10, maxHeight: 10) {
-            ...GatsbyContentfulFluid_tracedSVG
-          }
-        }
-      }
-    }
-  }
-}
-`
