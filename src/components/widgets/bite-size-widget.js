abb9d38637b16c873740a219ff5a312127c7ae70
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-08-18 09:35:02 -0500

    commit

--- src/components/widgets/bite-size-widget.js
+++ src/components/widgets/bite-size-widget.js
@@ -18,6 +18,7 @@ export default function BiteSizeWidget() {
                 ...GatsbyContentfulFluid_tracedSVG
               }
             }
+            slug
           }
         }
       }
--- src/templates/video-post.js
+++ src/templates/video-post.js
@@ -3,6 +3,11 @@ import { graphql } from 'gatsby'
 import { Helmet } from 'react-helmet'
 import get from 'lodash/get'
 import Layout from '../components/layout'
+import Donate from '../components/donate'
+import {EmailIcon, FacebookIcon, TwitterIcon} from 'react-share'
+
+import './video-post.css'
+import BiteSizeWidget from '../components/widgets/bite-size-widget'
 
 class VideoPostTemplate extends React.Component {
   render() {
@@ -13,18 +18,26 @@ class VideoPostTemplate extends React.Component {
       <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={`${video.title} | ${siteTitle}`} />
-          <div className="wrapper">
-            <h1 className="section-headline">{video.title}</h1>
-            <p
-              style={{
-                display: 'block',
-              }}
-            >
-              {video.publishDate}
-            </p>
+          <div className="wrapper video-post-wrapper">
             <div
               dangerouslySetInnerHTML={{__html: video.videoEmbed.childMarkdownRemark.html}}
             />
+            <h1 className="video-post-headline">{video.title}</h1>
+            <div className="video-meta">
+              <p className="video-contributor-name">{video.contributor.name}</p>
+              <p
+                style={{
+                  display: 'block',
+                }}
+              >
+                {video.publishDate}
+              </p>
+            </div>
+            <div>
+              <EmailIcon size={32} round={true}/>
+              <FacebookIcon size={32} round={true} className="mx-3"/>
+              <TwitterIcon size={32} round={true}/>
+            </div>
           </div>
         </div>
       </Layout>
@@ -43,6 +56,9 @@ export const pageQuery = graphql`
         childMarkdownRemark {
           html
         }
+      }
+      contributor {
+        name
       }
     }
   }
