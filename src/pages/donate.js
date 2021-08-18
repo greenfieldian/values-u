b837c475fce4ce3be6dd35062b6478b0e515b1f5
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-08-18 11:04:46 -0500

    donation page

--- src/pages/donate.js
+++ src/pages/donate.js
@@ -3,10 +3,13 @@ import DonorForm from '../components/donationForm/DonorForm'
 import Hero from '../components/hero'
 import Layout from '../components/layout'
 
+import './donate.css'
+
 const DonatePage = () => (
     <Layout>
-        <Hero />
-        <DonorForm />
+        <Hero title="Help Us Grow" description="Your contributions are crucial to our vital mission of spreading universal values and advancing social-cohesion, peace-building and solidarity throughout our global community."/>
+        {/*<DonorForm />*/}
+        <div className="donorbox-contain container"><script src="https://donorbox.org/widget.js" paypalExpress="false"></script><iframe src="https://donorbox.org/embed/donate-to-valuesu" name="donorbox" allowpaymentrequest="" seamless="seamless" frameborder="0" scrolling="no" height="600px" width="100%"></iframe></div>
     </Layout>
 )
 
