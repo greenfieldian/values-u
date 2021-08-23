1d6752a64cfe22e83b67bca7fe6fc2cabb01adf4
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-08-23 13:03:18 -0500

    commit

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -15,7 +15,7 @@ class BlogPostTemplate extends React.Component {
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
 
     return (
-      <Layout location={this.props.location}>hi
+      <Layout location={this.props.location}>
         <div style={{ background: '#fff' }} className="blog-post-container my-5">
           <Helmet title={`${post.title} | ${siteTitle}`} />
           <div>
