d2122537fb90ede20d6868a5511e08103e62434e
shockmousemedia <62213469+shockmousemedia@users.noreply.github.com>
2021-11-08 12:33:31 -0600

    test contact form

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -13,36 +13,36 @@ const ContactPage = () => (
         <Hero title="Contact Us" description="Have any questions, feeling inspired and want to get involved, or simply want to provide some feedback? Contact us through our online submission form - we would love to hear from you."/>
         <Container className="contact-form-container">
             <div>
-                <Form name="contact" method="post" data-netlify="true">
-                        <Form.Control type="hidden" name="form-name" value="contact"/>
-                    <Form.Group controlId="formFile" className="mb-3 form-item-flex">
+                <form name="contact" method="post" data-netlify="true">
+                        <input type="hidden" name="form-name" value="contact"/>
+                    <div className="mb-3 form-item-flex">
                         <Form.Label for="firstName">First Name</Form.Label>
                         <Form.Control type="text" name="firstName"/>
-                    </Form.Group>
-                    <Form.Group controlId="formFile" className="mb-3 form-item-flex">
+                    </div>
+                    <div className="mb-3 form-item-flex">
                         <Form.Label for="lastName">Last Name</Form.Label>
                         <Form.Control type="text" name="lastName"/>
-                    </Form.Group>
-                    <Form.Group controlId="formFile" className="mb-3 form-item-flex">
+                    </div>
+                    <div className="mb-3 form-item-flex">
                         <Form.Label for="phone">Phone</Form.Label>
                         <Form.Control type="tel" name="phone"/>
-                    </Form.Group>
-                    <Form.Group controlId="formFile" className="mb-3 form-item-flex">
+                    </div>
+                    <div className="mb-3 form-item-flex">
                         <Form.Label for="email">Email</Form.Label>
                         <Form.Control type="email" name="email"/>
-                    </Form.Group>
-                    <Form.Group className="contact-form-select">
+                    </div>
+                    <div className="contact-form-select">
                         <Form.Label for="subject">Subject</Form.Label>
                         <Form.Control type="text" name="subject"/>
-                    </Form.Group>
-                    <Form.Group controlId="formFile" className="mb-3 form-textarea">
-                        <Form.Label for="message">Message</Form.Label>
+                    </div>
+                    <div className="mb-3 form-textarea">
+                        <label for="message">Message</label>
                         <Form.Control type="textarea" as="textarea" rows={5} name="message"/>
-                    </Form.Group>
+                    </div>
                     <Button  type="submit" label="Submit">
                         Submit
                     </Button>
-                </Form>
+                </form>
             </div>
         </Container>
         <Donate />
